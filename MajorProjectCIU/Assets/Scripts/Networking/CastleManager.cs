7e569ffd0c61ea26d4dee3c1525f059d7508b3c2
Aaron <67357552+AyeAyeRon2@users.noreply.github.com>
2021-07-12 16:01:41 +1000

    added photon components to player controller

--- MajorProjectCIU/Assets/Scripts/Networking/CastleManager.cs
+++ MajorProjectCIU/Assets/Scripts/Networking/CastleManager.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Photon.Pun;
 
 public class CastleManager : MonoBehaviourPunCallbacks
 {
     public string playerPrefabLocation;
     public Transform spawnpoint;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            Application.Quit();
+        }
+    }
 
     public override void OnJoinedRoom()
     {
